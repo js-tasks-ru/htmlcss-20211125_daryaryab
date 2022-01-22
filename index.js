855d91b4023960058ec732774750169d18ee6c94
darya <darya@MacBook-Pro-darya.local>
2022-01-22 17:50:16 +0200

    resolve lection7

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 window.addEventListener('load', () => {
   initBurgerMenu();
+  initModalPopup();
   removePreload();
+});
+
+window.addEventListener('scroll', () => {
+  scrollToTop();
 });
 
 function initBurgerMenu() {
@@ -25,6 +30,45 @@ function initBurgerMenu() {
   }
 }
 
+function initModalPopup() {
+  const close = document.getElementById('modal-close');
+  const modal = document.getElementById('modal__popup');
+  const modalOpen = document.getElementById('modal-open');
+
+  if (!close || !modalOpen || !modal) {
+    return;
+  }
+
+  modalOpen.addEventListener('click', () => {
+    modal.style.display = 'block';
+    document.body.overflow = 'hidden';
+  });
+
+  close.addEventListener('click', () => {
+    modal.style.display = 'none';
+    document.body.overflow = 'initial';
+  });
+}
+
 function removePreload() {
   document.body.classList.remove('preload');
 }
+
+function scrollToTop() {
+  const arrow = document.getElementById('arrow-top');
+  const arrowWrapper = document.getElementsByClassName('page__navigate')[0];
+
+  if (!arrow) return;
+
+   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+     arrowWrapper.style.display = 'block';
+   } else {
+     arrowWrapper.style.display = 'none';
+   }
+
+   arrow.addEventListener('click', () => {
+     document.body.scrollTop = 0;
+     document.documentElement.scrollTop = 0;
+   });
+}
+
