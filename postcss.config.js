bd4feb5a382bc1a72ec988d1478676dd712d2d8c
darya <darya@MacBook-Pro-darya.local>
2022-01-25 10:34:19 +0200

    fix after review

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  syntax: 'postcss-scss',
-  map: { inline: true },
   plugins: [
     require('postcss-import'),
     require('postcss-nested'),
+    require('postcss-simple-vars'),
+    require('postcss-mixins'),
     require('autoprefixer'),
     require('cssnano'),
   ],
